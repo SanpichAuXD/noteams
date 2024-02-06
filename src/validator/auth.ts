156bf128c9ce1ad7da12c1df7323e3191a26ad23
Sanpis Poolsilpe <67864773+zeraph-christ@users.noreply.github.com>
2024-02-06 14:09:58 +0700

    🔥 fixed : validator

--- src/validator/auth.ts
+++ src/validator/auth.ts
@@ -1,8 +1,6 @@
 import { z } from "zod";
 const registerSchema = z.object({
     username: z.string().min(3).max(255),
-    firstname: z.string().min(3).max(255),
-    lastname: z.string().min(3).max(255),
     password: z.string().min(8).max(255),
     cfpassword : z.string().min(8).max(255),
     email: z.string().email(),
