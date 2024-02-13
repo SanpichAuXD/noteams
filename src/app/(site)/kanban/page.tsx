95d05a2deb8543c5371257c59d24bd01609fdfc1
Sanpis Poolsilpe <67864773+zeraph-christ@users.noreply.github.com>
2024-02-13 12:02:44 +0700

    ✏️ refactor : change file name and path

--- src/app/(site)/kanban/page.tsx
+++ src/app/(site)/kanban/page.tsx
@@ -1,12 +1,13 @@
-"use client"
+// "use client"
 import { KanbanBoard } from '@/components/kanban/KanbanBoard'
+import { cookies } from 'next/headers'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="mx-4 flex flex-col gap-6 p-10">
+    <main className="mx-4 flex flex-col gap-6 p-5">
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-              Drag and Drop Kanban Board
+              Drag and Drop Kanban Board 
             </h1>
             <KanbanBoard />
             <p className="leading-7 [&:not(:first-child)]:mt-6">
