c2472c7da64851f3075ae62990bfaa9cb4a5ba57
Sanpis Poolsilpe <67864773+zeraph-christ@users.noreply.github.com>
2024-02-20 10:12:28 +0700

    feat : Update layout sidebar and middleware

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,12 +1,11 @@
-"use client";
 
 import Sidenav from "@/components/sidenav/SliderBar";
 // Import global styles
 import "../globals.css";
 
 // Import necessary components and hooks
-import { useEffect, useState } from "react";
 import MobileHeader from "@/components/sidenav/MobileHeader";
+import Header from "@/components/sidenav/Header";
 
 // Define the RootLayout component
 export default function RootLayout({
@@ -15,41 +14,35 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   // State to track whether the sidebar is open or closed
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+  // const [sidebarOpen, setSidebarOpen] = useState(false);
 
   // State to track if the viewport is in mobile mode
-  const [isMobile, setIsMobile] = useState(false);
+  // const [isMobile, setIsMobile] = useState(false);
 
   // Effect to handle resizing and update isMobile state accordingly
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 1024);
+  // useEffect(() => {
+  //   const handleResize = () => setIsMobile(window.innerWidth < 1024);
 
-    // Initial resize check and event listener setup
-    handleResize();
-    window.addEventListener("resize", handleResize);
+  //   // Initial resize check and event listener setup
+  //   handleResize();
+  //   window.addEventListener("resize", handleResize);
 
-    // Cleanup: remove event listener on unmount
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  //   // Cleanup: remove event listener on unmount
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, []);
 
   return (
     
         <div className="flex h-screen bg-gray-200">
-          <div>
-            {/* Render the Sidenav component */}
-            <Sidenav
-              sidebarOpen={sidebarOpen}
-              setSidebarOpen={setSidebarOpen}
-            />
-          </div>
+          <Header />
           <div className="relative flex flex-col flex-1 lg:overflow-y-auto lg:overflow-x-hidden">
             {/* Render the Header component if in mobile mode */}
-            {isMobile && (
+            {/* {isMobile && (
               <MobileHeader
                 setSidebarOpen={setSidebarOpen}
                 className="sticky top-0 bg-white border-b border-slate-200 z-30"
               />
-            )}
+            )} */}
             {/* Render the main content */}
             <main>{children}</main>
           </div>
