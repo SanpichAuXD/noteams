ec6e7f533ca28bf536478accb01798452cf6f1b1
Squid3rd <88415664+Squid3rd@users.noreply.github.com>
2024-02-24 13:15:21 +0700

    update Profile
    Add mock data

--- src/app/(site)/profile/editprofile/page.tsx
+++ src/app/(site)/profile/editprofile/page.tsx
@@ -5,8 +5,31 @@ import { useRouter } from "next/router";
 import React, { useState } from "react";
 import ModalConfirm from "@/components/profilecomponent/savemodal";
 
-function editprofile() {
-  
+type Profile1 = {
+  userid: number;
+  username: string;
+  dob: Date | string;
+  bio: string;
+  email: string;
+  phone: string;
+  avatar: string;
+};
+
+type Team1 = {
+  team_id : string;
+  teamName : string;
+  team_poster: string;
+  role : string;
+};
+
+type Profileandteam = {
+  pData1: Profile1[];
+  pTeam: Team1[];
+};
+
+const EditProfile: React.FC<Profile1> = ({
+  userid, username, dob, bio, phone, avatar, email 
+}) => (
   // eslint-disable-next-line react-hooks/rules-of-hooks
   // const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -17,71 +40,85 @@ function editprofile() {
   //   console.log('Save action performed');
   //   closeModal();
   // };
-  return (
-    <main>
-      <p className="font-bold text-5xl p-3 underline underline-offset-8">
-        EditProfile
-      </p>
-      <div className="grid grid-cols-2 gap-4 m-16">
-        <div className=" m-16 ">
-          <div className="justify-center flex ">
-            <img
-              className="h-[24rem] w-[24rem] border-2 rounded-3xl"
-              src="https://static-00.iconduck.com/assets.00/profile-circle-icon-2048x2048-cqe5466q.png"
+
+  <main>
+    <p className="font-bold text-3xl p-3 underline underline-offset-8">
+      EditProfile
+    </p>
+    <div className="grid grid-cols-2 gap-4 m-16">
+      <div className=" m-16 ">
+        <div className="justify-center flex ">
+          <img
+            className="h-[18rem] w-[18rem] border-2 rounded-3xl"
+            src={avatar}
+          />
+        </div>
+        <div className="flex justify-center mt-10">
+          <button className="p-5 bg-green-300 hover:bg-green-600 text-1xl">
+            Edit Picture
+          </button>
+        </div>
+      </div>
+      <div className="grid content-center">
+        <form className="shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="w-full apx-3 mb-6 ">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-1xl font-bold mb-2"
+              htmlFor="grid-first-name"
+            >
+              Username
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-black rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+              placeholder={username}
             />
           </div>
-          <div className="flex justify-center mt-10">
-            <button className="p-5 bg-green-300 hover:bg-green-600 text-3xl">
-              Edit Picture
-            </button>
+          <div className="w-full apx-3 mb-6 ">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-1xl font-bold mb-2"
+              htmlFor="grid-first-name"
+            >
+              Date of Birth
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-black rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+              placeholder={dob.toString()}
+            />
           </div>
-        </div>
-        <div className="grid content-center">
-          <form className="shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <div className="w-full apx-3 mb-6 ">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-2xl font-bold mb-2"
-                htmlFor="grid-first-name"
-              >
-                First Name
-              </label>
-              <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-black rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="grid-first-name"
-                type="text"
-                placeholder="Jane"
-              />
-            </div>
-            <div className="w-full apx-3 mb-6 ">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-2xl font-bold mb-2"
-                htmlFor="grid-first-name"
-              >
-                Last Name
-              </label>
-              <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-black rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="grid-first-name"
-                type="text"
-                placeholder="Jane"
-              />
-            </div>
-            <div className="w-full apx-3 mb-6 ">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-2xl font-bold mb-2"
-                htmlFor="grid-first-name"
-              >
-                Email
-              </label>
-              <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-black rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                id="grid-first-name"
-                type="text"
-                placeholder="Jane"
-              />
-            </div>
-            <div className="flex justify-end">
-              <Link href="/profile">
+          <div className="w-full apx-3 mb-6 ">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-1xl font-bold mb-2"
+              htmlFor="grid-first-name"
+            >
+              Bios
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-black rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+              placeholder={bio}
+            />
+          </div>
+          <div className="w-full apx-3 mb-6 ">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-1xl font-bold mb-2"
+              htmlFor="grid-first-name"
+            >
+              Phone
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-black rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+              placeholder={phone}
+            />
+          </div>
+          <div className="flex justify-end">
+            <Link href="/profile">
               <button
                 // onClick={openModal}
                 className="bg-green-300 hover:bg-green-600 p-3"
@@ -89,18 +126,57 @@ function editprofile() {
                 Save Change
               </button>
               {/* <ModalConfirm isOpen={isModalOpen} onClose={closeModal} onConfirm={handleConfirm} /> */}
-              </Link>
-              <Link href="/profile">
-                <button className="bg-red-300 hover:bg-red-600 p-3 ml-5">
-                  Cancel
-                </button>
-              </Link>
-            </div>
-          </form>
-        </div>
+            </Link>
+            <Link href="/profile">
+              <button className="bg-red-300 hover:bg-red-600 p-3 ml-5">
+                Cancel
+              </button>
+            </Link>
+          </div>
+        </form>
       </div>
-    </main>
+    </div>
+  </main>
+);
+
+function editprofileApp() {
+  const profile01: Profile1[] = [
+    {
+      userid: 1,
+      username: "Peter1234",
+      dob: "12-12-2002",
+      bio: "bio XD",
+      phone: "0944215180",
+      email: "peter1234@gmail.comm",
+      avatar:
+        "https://static-00.iconduck.com/assets.00/profile-circle-icon-2048x2048-cqe5466q.png",
+    },
+  ];
+
+  const teamprofile01: Team1[] = [
+    {
+      team_id: "T00001",
+      teamName: "team1",
+      team_poster:
+        "https://media.licdn.com/dms/image/D4E12AQFP9XcUm3C_Fw/article-cover_image-shrink_600_2000/0/1684536742731?e=2147483647&v=beta&t=-87dzXNxDzNHcI-b69NtYenHzRCnjprXWM_Jq1JD1LM",
+      role: "Owner",
+    },
+    {
+      team_id: "T00002",
+      teamName: "team2",
+      team_poster:
+        "https://www.proofhub.com/articles/wp-content/uploads/2019/07/Why-Bigger-Team-Isn%E2%80%99t-Always-the-Better-Choice.jpeg",
+      role: "Member",
+    },
+  ];
+
+  return (
+    <div>
+        {profile01.map((prof) => (
+        <EditProfile key={prof.userid} {...prof} />
+      ))}
+    </div>
   );
 }
 
-export default editprofile;
+export default editprofileApp;
--- src/app/(site)/profile/page.tsx
+++ src/app/(site)/profile/page.tsx
@@ -6,73 +6,133 @@ import { MdEmail } from "react-icons/md";
 import { FaBirthdayCake, FaPhoneAlt } from "react-icons/fa";
 import Teaminprofile from "@/components/create/teaminprofile";
 import Link from "next/link";
+import { date } from "zod";
 
-function Profile() {
+type Profile1 = {
+  userid: number;
+  username: string;
+  dob: Date | string;
+  bio: string;
+  email: string;
+  phone: string;
+  avatar: string;
+};
+
+type Team1 = {
+  team_id: string;
+  teamName: string;
+  team_poster: string;
+  role: string;
+};
+
+type Profileandteam = {
+  pData1: Profile1[];
+  pTeam: Team1[];
+};
+
+const Profile: React.FC<Profileandteam> = ({ pData1, pTeam }) => (
+  <main className="min-h-screen max-h-screen">
+    {pData1.map((pData) => (
+      <div key={pData.userid}>
+        <p className="font-bold text-5xl p-3 underline underline-offset-8">
+          Profile
+        </p>
+
+        <div className="grid grid-cols-2 gap-4 m-16">
+          <div className=" m-16 ">
+            <div className="justify-center flex ">
+              <img
+                className="md:h-[20rem] md:w-[20rem] sm:h-[24rem] sm:w-[24rem] border-2 rounded-3xl"
+                src={pData.avatar}
+              />
+            </div>
+          </div>
+          <div className="m-16">
+            <div>
+              <p className="md:text-2xl font-bold">
+                {pData.username}
+                <Link href="/profile/editprofile">
+                  <button className="ml-10 bg-slate-700 p-2 rounded-md hover:bg-slate-300">
+                    <FaPencil />
+                  </button>
+                </Link>
+              </p>
+            </div>
+            <p className="md:text-1xl font-light">{pData.bio}</p>
+            <div className=" mt-24">
+              <p className="md:text-3xl font-bold">Contact</p>
+              <p className="mt-1 h-1 w-3/4 bg-black"></p>
+              <div className=" mt-6 flex items-center">
+                <p className="md:text-4xl mr-10">
+                  <MdEmail />
+                </p>
+                <span className="md:text-2xl">{pData.email}</span>
+              </div>
+              <div className=" mt-2 flex items-center">
+                <p className="text-4xl mr-10">
+                  <FaPhoneAlt />
+                </p>
+                <span className="md:text-2xl">{pData.phone}</span>
+              </div>
+              <div className=" mt-2 flex items-center">
+                <p className="text-4xl mr-10">
+                  <FaBirthdayCake />
+                </p>
+                <span className="md:text-2xl">{pData.dob.toString()}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    ))}
+    {/* <p className="text-5xl font-bold ">Organize</p> */}
+    <div className="flex justify-center">
+      <p className="mt-1 h-1 w-5/6 bg-black"></p>
+    </div>
+    <div className="mb-10">
+
+      <Teaminprofile pTeam={pTeam} />
+    </div>
+    
+  </main>
+);
+
+function ProfileApp() {
+  const profile01: Profile1[] = [
+    {
+      userid: 1,
+      username: "Peter1234",
+      dob: "12-12-2002",
+      bio: "bio XD",
+      phone: "0944215180",
+      email: "peter1234@gmail.comm",
+      avatar:
+        "https://static-00.iconduck.com/assets.00/profile-circle-icon-2048x2048-cqe5466q.png",
+    },
+  ];
+
+  const teamprofile01: Team1[] = [
+    {
+      team_id: "T00001",
+      teamName: "team1",
+      team_poster:
+        "https://media.licdn.com/dms/image/D4E12AQFP9XcUm3C_Fw/article-cover_image-shrink_600_2000/0/1684536742731?e=2147483647&v=beta&t=-87dzXNxDzNHcI-b69NtYenHzRCnjprXWM_Jq1JD1LM",
+      role: "Owner",
+    },
+    {
+      team_id: "T00002",
+      teamName: "team2",
+      team_poster:
+        "https://www.proofhub.com/articles/wp-content/uploads/2019/07/Why-Bigger-Team-Isn%E2%80%99t-Always-the-Better-Choice.jpeg",
+      role: "Member",
+    },
+  ];
+
   return (
-    <main className="min-h-screen max-h-screen">
-      <p className="font-bold text-5xl p-3 underline underline-offset-8">
-        Profile
-      </p>
-
-      <div className="grid grid-cols-2 gap-4 m-16">
-        <div className=" m-16 ">
-          <div className="justify-center flex ">
-            <img
-              className="md:h-[20rem] md:w-[20rem] sm:h-[24rem] sm:w-[24rem] border-2 rounded-3xl"
-              src="https://i.imgflip.com/4/2b5p.jpg"
-            />
-          </div>
-        </div>
-        <div className="m-16">
-          <div>
-            <p className="md:text-2xl font-bold">
-              NONTAPAT PAKER
-              <Link href="/profile/editprofile">
-                <button className="ml-10 bg-slate-700 p-2 rounded-md hover:bg-slate-300">
-                  <FaPencil />
-                </button>
-              </Link>
-            </p>
-          </div>
-          <p className="md:text-1xl font-light">Bachelor IT</p>
-          <div className=" mt-24">
-            <p className="md:text-3xl font-bold">Contact</p>
-            <p className="mt-1 h-1 w-3/4 bg-black"></p>
-            <div className=" mt-6 flex items-center">
-              <p className="md:text-4xl mr-10">
-                <MdEmail />
-              </p>
-              <span className="md:text-2xl">Peter987@kmitl.ac.th</span>
-            </div>
-            <div className=" mt-2 flex items-center">
-              <p className="text-4xl mr-10">
-                <FaPhoneAlt />
-              </p>
-              <span className="md:text-2xl">092-236547</span>
-            </div>
-            <div className=" mt-2 flex items-center">
-              <p className="text-4xl mr-10">
-                <FaBirthdayCake />
-              </p>
-              <span className="md:text-2xl">07/07/1991</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <p className="text-5xl font-bold ">Organize</p> */}
-      <div className="flex justify-center">
-        <p className="mt-1 h-1 w-5/6 bg-black"></p>
-      </div>
-      <div className="grid grid-cols-4 gap-4 m-16 mt-8">
-        {/* <Teaminprofile />
-        <Teaminprofile />
-        <Teaminprofile />
-        <Teaminprofile />
-        <Teaminprofile />
-        <Teaminprofile /> */}
-      </div>
-    </main>
+    <div>
+      <Profile pTeam={teamprofile01} pData1={profile01} />
+    </div>
   );
 }
 
-export default Profile;
+export default ProfileApp;
