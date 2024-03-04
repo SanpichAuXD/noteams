92e66fcc2fd79bb577b5aab5e92293b007af383b
Sanpis Poolsilpe <67864773+zeraph-christ@users.noreply.github.com>
2024-03-04 21:55:24 +0700

    🔥 fixed : remove import and commented

--- src/components/file/upload-dnd.tsx
+++ src/components/file/upload-dnd.tsx
@@ -4,7 +4,6 @@ import { DataTable } from "@/components/file/data-table";
 import React, { useEffect, useRef, useState } from "react";
 import { useToast } from "../ui/use-toast";
 // import { toast } from "sonner"
- import { useFileStore } from './../../store/FileStore';
 import { Files, Upload } from "lucide-react";
 import { Button } from "../ui/button";
 import UploadButton from "./UploadButton";
@@ -30,14 +29,12 @@ const UploadDnd = (
 	console.log(data, 'hydrate file')
 	const [dragActive, setDragActive] = useState<boolean>(false);
 	const inputRef = useRef<any>(null);
-	const {addFile,files} = useFileStore()
 	const [files1, setFiles] = useState<any>([]);
     const {toast} = useToast()
 	// useEffect(() => {
 	// 	setFilestate(data)
 	// }, [data,	setFilestate]);
 	const role = 'member'
-	console.log(files)
 	function handleChange(e: any) {
 		e.preventDefault();
 		console.log("File has been added");
@@ -49,27 +46,6 @@ const UploadDnd = (
 		}
 	}
 
-	// async function handleSubmitFile(e: any) {
-	// 	const formData = new FormData();
-	// 	if (files1.length === 0) {
-	// 		// no file has been submitted
-	// 	} else {
-	// 		// write submit logic here
-	// 		files.forEach((file: File) => {
-	// 			console.log(typeof file, "check type file");
-	// 			formData.append("files", file);
-	// 		});
-	// 		console.log(formData.get("file"));
-	// 		const response = await fetch(
-	// 			`${process.env.NEXT_PUBLIC_API_PROD_URL}/files/upload`,
-	// 			{
-	// 				method: "POST",
-	// 				body: formData,
-	// 			}
-	// 		);
-	// 		console.log(response);
-	// 	}
-	// }
 	const queryClient = useQueryClient();
 
 	const mutation = useMutation<TeamFile[], IFormattedErrorResponse, FormData>({
@@ -120,16 +96,7 @@ const UploadDnd = (
 		setDragActive(true);
 	}
 
-	function removeFile(fileName: any, idx: any) {
-		const newArr = [...files];
-		newArr.splice(idx, 1);
-		setFiles([]);
-		setFiles(newArr);
-	}
-	function openFileExplorer() {
-		inputRef.current.value = "";
-		inputRef.current.click();
-	}
+	
 	return (
 		<div className="w-full max-h-min">
 			<form
