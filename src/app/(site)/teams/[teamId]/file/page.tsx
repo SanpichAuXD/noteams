0a169a166c784e1854a9ae35dd8ac3ecc539e4c5
Sanpis Poolsilpe <67864773+zeraph-christ@users.noreply.github.com>
2024-03-05 21:35:01 +0700

    fixed import

--- src/app/(site)/teams/[teamId]/file/page.tsx
+++ src/app/(site)/teams/[teamId]/file/page.tsx
@@ -1,14 +1,20 @@
-"use client"
 import { useRef, useState } from "react";
 import { FileTable, columns } from "@/components/file/column";
 import { DataTable } from "@/components/file/data-table";
 import UploadDnd from "@/components/file/upload-dnd";
-import { useFileStore } from "@/store/FileStore";
-export default function DragAndDrop() {
-  
+import { QueryClient } from "@tanstack/react-query";
+import { getFiles } from "@/api-caller/file";
+import { cookies } from "next/headers";
+export default async function FilePage({ params }: { params: { teamId: string } }) {
+  const token = cookies().get("accessToken")?.value!;
+  const queryClient  = new QueryClient();
+	await queryClient.prefetchQuery({
+		queryKey: [`hydrate-file-${params.teamId}`],
+		queryFn: ()=> getFiles({token:token, team_id:params.teamId}),
+	  });
   return (
     <div className="flex items-center justify-center bg-blue-700">
-      <UploadDnd  />
+      <UploadDnd token={token} team_id={params.teamId} />
     </div>
   );
 }
